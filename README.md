03e300794eef1d5c8952083c2bae9096836b0b82
harinikanand <harinikanand03@gmail.com>
2015-12-28 22:59:09 -0600

    FIRST COMMIT

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-test
+Project Name: Neighborhood Map
+
+Author: Harini Anand
+
+Date: 12/28/2015
+
+Prerequisites:
+==============
+1. Download this project (Neighborhood Map) from github
+
+Instructions to view the Neighborhood Map:
+=========================================
+1. Open the folder containing the project
+2. Locate the index.html file in the folder
+3. Double click on the index.html 
+4. Alternately, you could also open a google chrome browser 
+and drag and drop the index.html in the folder on to it.
+
+Description:
+============
+As part of the neighborhood Map project, I implemented a Map
+to display the various forest preserves areas in Chicago area.
+
+Opening the index.html will display a page that on the left hand shows the
+list of locations (forest preserve areas) along with a filter/search option, 
+in the middle a chicago area Map
+and to the right two boxes that are stacked and they display the wikipedia and yelp
+reviews of the forest preserve area selected and others attractions near it 
+respectively.
+
+The filter option enables user to enter characters based on the locations in the
+list view are filtered. At the same time, the map in the middle is updated to only show the filtered locations along with infoWindow showing address, latitude, longitude and a streetview image.
+If the filtration results in only one location, then relevant wikipedia and yelp reviews about the location and other attractions near it on the right hand side.
+
+
+Clicking on location updates the map view only show selected location with infoWindow opened displaying address, latitude, longitude and a streetview image.
+It also shows the relevant wikipedia and yelp reviews about the location and other
+attractions near it on the right hand side.
+
+
+Sources and References:
+1. Background image: http://wallpaperswide.com/download/dirt_road_through_forest-wallpaper-1280x800.jpg
+2. Chicago area forest preserves: google search results
+3. Google Maps & Knockout js: http://jsfiddle.net/rniemeyer/FcSmA/
+                              http://www.hoonzis.com/knockoutjs-and-google-maps-binding/
+                              http://jsfiddle.net/Wt3B8/23/                             
+4. Google streetview image: https://support.google.com/fusiontables/answer/2796058?hl=en
+5. Wikipedia reviews (geosearch): https://www.mediawiki.org/wiki/API:Showing_nearby_wiki_information
+6. Yelp reviews: http://forums.asp.net/t/1801674.aspx?how+to+create+API+URL+using+some+credentials
+                 https://www.yelp.com/developers/documentation/v2/search_api
+                 https://github.com/brettdavis4/udacity_js_design_patterns/blob/master/final/js/yelp.js
+
+Important Note: For some forest preserve areas, there is no street view image available.
---
+++ js/app.js
@@ -0,0 +1,487 @@
+// Array to store information about forest preserve areas in Chicago
+// stores name, street, city, latitude and longitude
+var mapMarkerData = [
+{ 
+    name: 'Dan Ryan Woods',
+    street: '8300 S Western Ave',
+    city: 'Chicago, IL 60620',
+    latitude: 41.736284,
+    longitude: -87.679113
+},
+{
+    name: 'Thatcher Woods',
+    street: '8030 Chicago Ave',
+    city: 'Chicago, IL 60153',
+    latitude: 41.906679,
+    longitude: -87.828845
+},
+{
+    name: 'Thaddeus S. Lechowicz Woods',
+    street: '5901 N Central Ave',
+    city: 'Chicago, IL 60646',
+    latitude: 41.994171,
+    longitude: -87.762057
+},
+{
+    name: 'Jerome Huppert Woods',
+    street: '536 N Harlem Ave',
+    city: 'River Forest, IL 60305',
+    latitude: 41.918359,
+    longitude: -87.831988
+},
+{
+    name: 'Edgebrook Woods',
+    street: '6129 N Central Ave',
+    city: 'River Forest, IL 60646',
+    latitude: 41.996465,
+    longitude: -87.763633
+},
+{
+    name: 'Portage Woods',
+    street: 'W 47th St',
+    city: 'Berwyn, IL 60402',
+    latitude: 41.810603,
+    longitude: -87.804270
+},
+{
+    name: 'LaBagh Woods',
+    street: '4600-4628 W Foster Ave',
+    city: 'Chicago, IL 60630',
+    latitude: 41.979648,
+    longitude: -87.743775
+},
+{
+    name: 'Forest Glen Woods Picnic Grove',
+    street: '5400 N Lawler Ave',
+    city: 'Chicago, IL 60630',
+    latitude: 41.980755,
+    longitude: -87.755739
+},
+{
+    name: 'Irene C. Hernandez Picnic Grove',
+    street: '4498 Foster Ave',
+    city: 'Chicago, IL 60630',
+    latitude: 41.976177,
+    longitude: -87.740006
+},
+{
+    name: 'Fullerton Woods',
+    street: '8413 W Fullerton Ave',
+    city: 'River Grove, IL 60171',
+    latitude: 41.920529,
+    longitude: -87.839088
+},
+{
+    name: 'Catherine Chevalier Woods',
+    street: '5564 N East River Rd',
+    city: 'Chicago, IL 60706',
+    latitude: 41.976463,
+    longitude: -87.852520
+},
+{
+    name: 'Schiller Woods',
+    street: 'Schiller Woods South',
+    city: 'Schiller Park, IL 60634',
+    latitude: 41.946759,
+    longitude: -87.847609
+},
+{
+    name: 'Eggers Grove',
+    street: '11200 S Ave E',
+    city: 'Chicago, IL 60617',
+    latitude: 41.946623,
+    longitude: -87.847555
+},
+{
+    name: 'Bunker Hill',
+    street: 'W Harts Rd',
+    city: 'Chicago, IL 60714',
+    latitude: 42.006768,
+    longitude: -87.789930
+},
+{
+    name: 'Harms Woods',
+    street: '352 Harms Rd',
+    city: 'Skokie, IL 60077',
+    latitude: 42.060047,
+    longitude: -87.770280
+},
+{
+    name: 'Beaubien Forest Preserve',
+    street: '1 W Doty Ave S',
+    city: 'Chicago, IL 60633',
+    latitude: 41.649450,
+    longitude: -87.585304
+},
+{
+    name: 'White Eagle Woods',
+    street: '7317 40th St',
+    city: 'Lyons, IL 60534',
+    latitude: 41.822695,
+    longitude: -87.806113
+},
+{
+    name: 'Fullersburg County Forest Preserve',
+    street: '3609 Spring Rd',
+    city: 'Oak Brook, IL 60523',
+    latitude: 41.835876,
+    longitude: -87.934365
+},
+{
+    name: 'Hiawatha Park',
+    street: '8029 West Forest Preserve Drive',
+    city: 'Chicago, IL 60634',
+    latitude: 41.948014,
+    longitude: -87.825467
+},
+{
+    name: 'McCormick Woods',
+    street: 'S 1st Ave',
+    city: 'Riverside, IL 60546',
+    latitude: 41.844126,
+    longitude: -87.828845
+},
+{
+    name: 'William W. Powers Picnic Park',
+    street: '12949 S Avenue O',
+    city: 'Chicago, IL 60633',
+    latitude: 41.666936,
+    longitude: -87.524253
+}]
+
+// Global array variable to hold makers
+var markers = [];
+// Global array variable to hold infoWindows onjects corresponding to markers
+var infoWindows = [];
+// Global array variable to hold content to show when marker is clicked
+var infoWindows_content = [];
+// Global variable to indicate whether map api is initialized
+var mapInitialized = false;
+// Global variable to hold the map object
+var globalMap;
+// points to wikipedia-links document element in the HTML DOM
+var $wikiElem = $('#wikipedia-links');
+// points to yelp-links document element in the HTML DOM
+var $yelpElem = $('#yelp-links');
+
+// MapMarker object which is part of the data Model
+// It contains name, street, city, latitude
+// longitude as knockout js observerables - bound to HTML elements
+// address is a knockout computed observable that contains street 
+// and city concatenated with comma in between.
+// latlon is a knockout computed observable that contains 
+// latitude and longitude concatenated with |.
+// latlon2 is a knockout computed observable that contains 
+// latitude and longitude concatenated with a comma in between. 
+var MapMarker = function(data) {
+    this.name = ko.observable(data.name);
+    this.street = ko.observable(data.street);
+    this.city = ko.observable(data.city);
+
+    this.address = ko.computed(function() {
+        return this.street() + ", "+ this.city();
+    }, this);
+
+    this.latitude = ko.observable(data.latitude);
+    this.longitude = ko.observable(data.longitude);
+
+    this.latlon = ko.computed(function() {
+        return this.latitude() + "|" + this.longitude();
+    }, this);
+
+    this.latlon2 = ko.computed(function() {
+        return this.latitude() + "," + this.longitude();
+    }, this);
+}
+
+// ViewModel:
+// 1. It creates the markerList - a knockout observable array
+// which stores the map markers for all the forest preserve areas
+// listed above.
+// 2. It creates and maintains the filteredMarkers - a knockout computed observable array
+// which is derived from the markerList based on matching with the filter/search string.
+// filteredMarkers is dependent on the filter string.
+var ViewModel = function() {
+    var self = this;
+
+    //create markerList, an observable Array with the MapMarker objects
+    //corresponding to all the forest preserve areas listed above
+    this.markerList = ko.observableArray([]);
+    mapMarkerData.forEach(function(mapItem){
+        self.markerList.push(new MapMarker(mapItem));
+    });
+
+    // stringStartsWith is a function that returns true or false based on 
+    // whether string starts with startsWith
+    var stringStartsWith = function (string, startsWith) {
+    string = string || "";
+    if (startsWith.length > string.length)
+        return false;
+    return string.substring(0, startsWith.length) === startsWith;
+    };
+
+    // filter - a observable that contains the filter search string
+    this.filter = ko.observable("");
+
+    // filteredMarkers - a computed observable array based on:
+    // 1. if fitler is not set, returns markerList as is
+    //    along with that, if map is already initialized, it sets all the markers to be visible
+    //    sets the marker animation to DROP and closes any open infoWindows for the markers.
+    //    It also resets the wikipedia results and yelp reviews to show default strings.
+    // 2. if fitler is set then it first determines all the markers in MarkerList that begin with it.
+    //    following that, if map is initialized, it identifies and only shows those markers on map that begin with the filter string
+    //    Changes the marker animation to BOUNCE and also shows the infowindows for only those markers.
+    //    if the match is only one marker, then it also shows the wikipedia results and yelp reviews for that one forest preserve.
+    // This leads to display fitlered list and showing only the filtered markers on the map.
+    this.filteredMarkers = ko.computed(function(){
+        var filter = this.filter().toLowerCase();
+        if(!filter) {
+            if (mapInitialized == true) {
+               for (i = 0; i < markers.length; ++i) {
+                    markers[i].setVisible(true);
+                    markers[i].setAnimation(google.maps.Animation.DROP);
+                    infoWindows[i].close(globalMap, markers[i]);
+                }
+                $wikiElem.text("Select a Forest Preserve in the list and find relevant Wikipedia articles here!");
+                $yelpElem.text("Select a Forest Preserve in the list and find relevant Yelp articles/reviews here!");
+            }
+            return this.markerList();
+        } else {
+            var newMarkerList = ko.utils.arrayFilter(this.markerList(), function(marker) {
+                return stringStartsWith(marker.name().toLowerCase(), filter);
+            });
+            
+            if (mapInitialized == true) {
+                for (i = 0; i < markers.length; ++i) {
+                    markers[i].setVisible(true);
+                }
+                for (i = 0; i < markers.length; ++i) {
+                    var found = false;
+                    var titleName = markers[i].getTitle();
+                    for (j = 0; j < newMarkerList.length;++j) {
+                       if (titleName === newMarkerList[j].name()) {
+                          found = true;
+                       }
+                    }
+                    if (found == false) {
+                        markers[i].setVisible(false); // marker not is filtered list, so do no show it on map
+                        infoWindows[i].close(globalMap, markers[i]); // close the infoWindow 
+                    } else { // marker in filtered list
+                        markers[i].setAnimation(google.maps.Animation.BOUNCE); // set marker animation to Bounce
+                        infoWindows[i].setContent(infoWindows_content[i]); // set and show the infoWindow for the marker
+                        infoWindows[i].open(globalMap, markers[i]);
+                    }
+                }
+                if (newMarkerList.length == 1) { // only one marker in filtered list, so show wikiLinks and Yelp reviews
+                    displayWikiLinks(newMarkerList[0].latlon());
+                    displayYelpLinks(newMarkerList[0].address(), newMarkerList[0].latlon2(), newMarkerList[0].name());
+                } else { // Multiple markers in the list, so show default strings for wikiLinks and yelp reviews
+                    $wikiElem.text("Select a Forest Preserve in the list and find relevant Wikipedia articles here!");
+                    $yelpElem.text("Select a Forest Preserve in the list and find relevant Yelp articles/reviews here!");
+                }
+            }
+           return newMarkerList;
+        }
+     }, this);
+
+    // displayWikiLinks = shows the Wikipedia results for a given latlon
+    // used the geosearch option in the media wiki api
+    // Implemented Error handing if response does not return within 8000ms
+    // used ajax jsonp query with the url
+    // the reponse is filtered, formatted and displayed as part of wikipedia-links
+    // DOM element
+    function displayWikiLinks(latlon) {
+        //start with setting empty string to the wikipedia-links element
+        $wikiElem.text("");
+
+        // Plug in the latlon provided in the wiki media geosearch URL
+        var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gslimit=10&gscoord='+latlon+'&format=json&callback=?';
+        // Error handling - if no response, an error is shown
+        var wikiRequestTimeout = setTimeout(function() {
+            $wikiElem.text("failed to get wikipedia results");
+        }, 8000);
+
+        $.ajax({
+            url: wikiUrl,
+            dataType: 'jsonp',
+            success: function(response) {
+                var articleList = response.query["geosearch"];
+
+                for (var i=0;i < articleList.length; i++) {
+                    articlePageId = articleList[i].pageid;
+                    articleTitle = articleList[i].title;
+                    var url = 'https://en.wikipedia.org/?curid=' + articlePageId;
+                    $wikiElem.append('<li><a href="'+url+'">'+articleTitle+'</a></li>');
+                }
+                clearTimeout(wikiRequestTimeout);
+            }
+        });
+    }
+
+    // displayYelpLinks = shows the Yelp reviews based on a given address, latlon and name
+    // used the Yelp API v2.0 with OAUTH to query parks category with the above criteria
+    // Implemented Error handing if response does not return within 8000ms
+    // used ajax jsonp query with the url
+    // the reponse is filtered, formatted and displayed as part of yelp-links
+    // DOM element
+    function displayYelpLinks(address, latlon2, name) {
+        // auth object to hold OAUTH 1.0 paramters
+        var auth = { 
+            consumerKey: "xWuLN66vI9E6iIqZZiFaUw", 
+            consumerSecret: "dm_9nosRLIE-7KV7jWNR6hNcXiA",
+            accessToken: "L4hQysaiwzPBkkmIRZO_3OHRs_zmlL_w",
+            accessTokenSecret: "v5p1MyECZrslYhjyN4FDjENpsdM",
+            serviceProvider: {
+                    signaureMethod: "HMAC-SHA1"
+                }
+            };
+        // parameters to search based on
+        var category = 'parks';
+        var address = address;
+        var cll = latlon2;
+        var name = name;
+        var accessor = {
+            consumerSecret: auth.consumerSecret,
+            tokenSecret: auth.accessTokenSecret
+        };
+        // All the search and oauth parameters are pushed to an array
+        var parameters = [];
+        parameters.push(['category_filter', category]);
+        parameters.push(['location', address]);
+        parameters.push(['cll',cll]);
+        parameters.push(['name',name]);
+        parameters.push(['callback', 'cb']);
+        parameters.push(['oauth_consumer_key', auth.consumerKey]);
+        parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+        parameters.push(['oauth_token', auth.accessToken]);
+        parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+        var message = { 
+            'action': 'http://api.yelp.com/v2/search',
+            'method': 'GET',
+            'parameters': parameters 
+        };
+
+        OAuth.setTimestampAndNonce(message);  
+        OAuth.SignatureMethod.sign(message, accessor);
+        var parameterMap = OAuth.getParameterMap(message.parameters);
+        parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
+
+        //start with setting empty string to the yelp-links element
+        $yelpElem.text("");
+
+        // Error handling - if no response, an error is shown
+        var yelpRequestTimeout = setTimeout(function() {
+            $yelpElem.text("failed to get Yelp reviews");
+        }, 60000);
+
+        $.ajax({
+            'url': message.action,
+            'data': parameterMap,
+            'cache': true,
+            'dataType': 'jsonp',
+            'type': 'get',
+            'success': function(data, textStats, XMLHttpRequest) {
+                var articleList = data["businesses"];
+
+                for (var i=0;i < articleList.length; i++) {
+                    var url =  articleList[i].url;
+                    $yelpElem.append('<li><a href="'+url+'">'+articleList[i].name+" "+'</a><img src="'+articleList[i].rating_img_url+'"></li>');
+                }
+                clearTimeout(yelpRequestTimeout);
+            },
+            'error': function(data, textStats, XMLHttpRequest) {
+                console.log('Yelp query did not work');
+                console.log(XMLHttpRequest);
+                //$yelpElem.text("failed to get Yelp reviews");
+            }
+        });
+    }
+
+    // displayMarker - the function is invoked when a forest preserve item is clicked
+    this.displayMarker = function() {
+        //this function sets the filter to the name of the forest preserve.
+        //Due to binding, this results in updated filteredMarker array which then
+        //only shows this one marker
+        self.filter(this.name());
+        //Display wikipedia results and yelp reviews
+        displayWikiLinks(this.latlon());
+        displayYelpLinks(this.address(), this.latlon2(), this.name());
+
+    };
+
+    // clear - the function is invoked when clear button is selected by the user
+    this.clear = function() {
+        // this function sets filter to empty string
+        this.filter("");
+    }
+}
+
+// Knockout js binding implementation for googlemap
+ko.bindingHandlers.googlemap = {
+    // init function - creates a google.maps.Map() object and initializes it
+    init: function (element, valueAccessor) {
+        var value = valueAccessor(),
+            mapOptions = {
+            zoom: 10,
+            panControl: false,
+            center: new google.maps.LatLng(value.centerLat, value.centerLon),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+            },
+            map = new google.maps.Map(element, mapOptions);
+          
+        globalMap = map;
+        // create markers for all the forest preserves
+        for (var l in value.locations())
+        {
+            var latLng = new google.maps.LatLng(
+                            value.locations()[l].latitude(),
+                            value.locations()[l].longitude());
+
+            var marker = new google.maps.Marker({
+                position: latLng,
+                animation: google.maps.Animation.DROP,
+                title: value.locations()[l].name(),
+                map: map
+              });
+            // push the marker to the global markers array
+            markers.push(marker);
+            // createa InfoWindow object
+            var infoWindow = new google.maps.InfoWindow();
+            // create the content string which shows the address, latitude, longitude and the streetview image
+            var content = '<div id="content">'+
+                           '<div id="siteNotice">'+
+                           '</div>'+
+                           '<h2 id="firstHeading" class="firstHeading">' + value.locations()[l].name() + '</h2>'+
+                           '<div id="bodyContent">'+
+                           '<p><b>Address: </b>' + value.locations()[l].address() +
+                           '<b> latitude: </b> '+ value.locations()[l].latitude() +
+                           '<b> longitude: </b>'+ value.locations()[l].longitude() + '</p><br><br>' +
+                           '<img src="http://maps.googleapis.com/maps/api/streetview?size=600x100&location='+value.locations()[l].address()+'"'+
+                           '</div>'+
+                           '</div>';
+            // Add a listener for mouseover event which will display the infowindow and also sets the animation to BOUNCE
+            google.maps.event.addListener(marker, 'mouseover', (function(marker,content,infoWindow) {
+                        return function() {
+                            infoWindow.setContent(content);
+                            infoWindow.open(map, marker);
+                            marker.setAnimation(google.maps.Animation.BOUNCE);
+                        }  
+            })(marker,content,infoWindow));
+            // Add a listener for mouseout event which will close the infowindow
+            google.maps.event.addListener(marker, 'mouseout', (function(marker,infoWindow) {
+                        return function() {
+                            infoWindow.close(map, marker);
+                            marker.setAnimation(google.maps.Animation.DROP);
+                        }  
+            })(marker,infoWindow));
+            // Save the infowindows objects and the content strings in global arrays
+            infoWindows_content.push(content);
+            infoWindows.push(infoWindow);
+        }
+        // Now that all markers, infowindows are created and map is initialized, set the mapInitialized variable to true
+        mapInitialized = true; 
+    }
+};
+
+ko.applyBindings(new ViewModel());
